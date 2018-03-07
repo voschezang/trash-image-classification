5dca257b6157485e78f3e26e1a71720fed0fa256
markvos <markvoschezang@gmail.com>
2018-03-07 22:26:03 +0100

    update dataset types

--- src/data.py
+++ src/data.py
@@ -1,14 +1,17 @@
-""" Functions specific to our dataset
+""" Functions that are specific to our dataset
 """
 import pandas
 import os, sklearn, skimage, pandas, numpy as np
 # from sklearn import svm
 # from skimage import data, io, filters
+from collections import namedtuple
 
 import config
 from utils import utils
 
-# from src.utils import utils # custom functions, in local environment
+# from utils import utils # custom functions, in local environment
+
+Dataset = namedtuple('Dataset', ['train', 'test', 'labels'])
 
 
 def init_dataset():
@@ -16,7 +19,7 @@ def init_dataset():
     labels = pandas.read_csv(config.dataset_dir + 'labels.csv')
     train = os.listdir(config.dataset_dir + 'train/')
     test = os.listdir(config.dataset_dir + 'test/')
-    return train, test, labels
+    return Dataset(train, test, labels)
 
 
 def dict_index_to_label(labels):
@@ -46,6 +49,24 @@ def read_img(folder='train', img_name='aed285c5eae61e3e7ddb5f78e6a7a977.jpg'):
         folder += '/'
     filename = config.dataset_dir + folder + img_name
     return skimage.io.imread(filename)
+
+
+# def extract_data(img_list):
+#     print('extract data:',len(img_list))
+#     global labels
+#     global dict_label_to_index
+#     img_data = []
+#     img_labels = []
+#     for img_name in img_list:
+#         img = data.read_img('train/',img_name)
+#         if img.shape == dimensions:
+#             img_data.append(img.flatten())
+#             breed = data.filename_to_class(labels,img_name)
+#             breed_index = dict_label_to_index[breed]
+#             img_labels.append(breed_index)
+# #         else:
+# #             print('dims')
+#     return (img_data, img_labels)
 
 
 def gen_random_img(example_img):
