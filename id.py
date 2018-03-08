460321a3375d48b2442b34a0ba455c2101bdc761
markvos <markvoschezang@gmail.com>
2018-03-08 09:51:10 +0100

    update

---
+++ id.py
@@ -0,0 +1,21 @@
+
+
+"""
+svm + nn
+
+noise toevoegen als negative training data
+
+abstracte vormen als training data?
+
+foto's distorten/buigen/rekken om data uit te bereiden
+
+edge-detection -> foto's croppen
+
+
+advanced
+
+generate images that activate the nn
+- use these images as negative examples to increase the amount of training data
+
+
+"""
